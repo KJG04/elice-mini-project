98f0d5423fbf293f06a71e00dde69b0a1236563e
KJG04 <freedom7113@gmail.com>
2022-04-23 13:26:58 +0900

    [FEAT] Publishing SearchArea UI
    SearchArea의 UI 작성을 완료한다

--- src/components/SearchArea/index.tsx
+++ src/components/SearchArea/index.tsx
@@ -1,5 +1,13 @@
+import * as S from "./styles";
+import icon from "../../assets/search-icon.svg";
+
 const SearchArea = () => {
-  return <div>this is header</div>;
+  return (
+    <S.Container>
+      <S.Icon src={icon} alt="검색 아이콘" />
+      <S.Input placeholder="배우고 싶은 언어, 기술을 검색해 보세요" />
+    </S.Container>
+  );
 };
 
 export default SearchArea;
--- src/components/SearchArea/styles.ts
+++ src/components/SearchArea/styles.ts
@@ -1,1 +1,24 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 12px 0px;
+  border: 1px solid rgb(201, 202, 204);
+  border-radius: 4px;
+  display: flex;
+`;
+
+export const Input = styled.input`
+  &::placeholder {
+    color: gray;
+  }
+  border: none;
+  outline: none;
+  flex: 1;
+`;
+
+export const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+  margin: 0px 16px;
+`;
