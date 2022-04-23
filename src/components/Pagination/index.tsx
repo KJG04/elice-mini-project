329b44ed1cf68e22f9e7e8e3141ac25646d1d316
KJG04 <freedom7113@gmail.com>
2022-04-23 21:28:11 +0900

    [FEAT] Publishing Pagination UI
    Pagination UI을 작성한다

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,38 @@
+import * as S from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+
 const Pagination = () => {
-  return <div>this is pagination</div>;
+  return (
+    <S.Container>
+      <div>
+        <S.ArrowContainer>
+          <FontAwesomeIcon width={14} height={14} icon={faAngleLeft} />
+        </S.ArrowContainer>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <div>
+        <S.PageButton>1</S.PageButton>
+      </div>
+      <S.ArrowContainer>
+        <FontAwesomeIcon icon={faAngleRight} />
+      </S.ArrowContainer>
+    </S.Container>
+  );
 };
 
 export default Pagination;
--- src/components/Pagination/styles.ts
+++ src/components/Pagination/styles.ts
@@ -1,1 +1,38 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin-top: 1.5rem;
+  justify-content: center;
+  display: flex;
+`;
+
+export const PageButton = styled.button`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  margin: 0px 6px;
+  padding: 0;
+  border: none;
+  color: #999;
+  font-weight: 700;
+  font-size: 0.875rem;
+  transition: all 150ms ease-in-out 0s;
+
+  &:hover {
+    color: #524fa1;
+  }
+
+  &.active {
+    color: white;
+    background-color: #524fa1;
+  }
+`;
+
+export const ArrowContainer = styled.button`
+  border: none;
+  padding: 0;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  margin: 0px 6px;
+`;
