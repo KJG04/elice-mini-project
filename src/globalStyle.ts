c61a57cd76b254f601c87e00bebb8acf43c0b0d8
KJG04 <freedom7113@gmail.com>
2022-04-23 12:25:33 +0900

    [FEAT] Add Reset CSS
    sanitize를 사용하여 스타일을 초기화 하였다

---
+++ src/globalStyle.ts
@@ -0,0 +1,14 @@
+import { css } from "@emotion/react";
+
+const globalStyle = css`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: auto;
+  }
+`;
+
+export default globalStyle;
