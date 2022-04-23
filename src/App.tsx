c509f1ff32808cdac792abbb86d687b5cb5c4b14
KJG04 <freedom7113@gmail.com>
2022-04-23 23:22:15 +0900

    [FEAT] Add Provider
    react query와 recoil을 위한 provider를 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Global } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 import { MainContainer } from "./container";
 import globalStyle from "./globalStyle";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Global styles={globalStyle} />
-      <MainContainer />
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <Global styles={globalStyle} />
+          <MainContainer />
+        </QueryClientProvider>
+      </RecoilRoot>
     </>
   );
 }
