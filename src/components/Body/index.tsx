c694eabbd753720eb89a4d0e1735b12f8b38fd92
KJG04 <freedom7113@gmail.com>
2022-04-23 14:17:36 +0900

    [FEAT] Publishing Body UI
    Body 컴포넌트의 UI를 작성한다

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,5 +1,20 @@
+import CourseCard from "../CourseCard";
+import * as S from "./styles";
+
 const Body = () => {
-  return <div>this is body</div>;
+  return (
+    <>
+      <S.Count>전체 77개</S.Count>
+      <S.Grid>
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </S.Grid>
+    </>
+  );
 };
 
 export default Body;
--- src/components/Body/styles.ts
+++ src/components/Body/styles.ts
@@ -1,1 +1,27 @@
-export {};
+import styled from "@emotion/styled";
+
+export const Count = styled.div`
+  font-size: 0.75rem;
+  color: #222;
+  font-weight: 700;
+  padding-bottom: 0.75rem;
+  padding-top: 0.75rem;
+  border-bottom: 1px solid rgb(225, 226, 228);
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media screen and (min-width: 769px) and (max-width: 1280px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (min-width: 576px) and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 575px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
